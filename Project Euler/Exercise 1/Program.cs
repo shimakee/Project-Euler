b8c99e1b2ba788e8d25dacbe4cc35bc124716586
Shimakee <shimakee@gmail.com>
2020-06-25 23:54:12 +0800

    Exercise 2: Sum of even fibonacci numbers under 4,000,000.

--- Project Euler/Exercise 1/Program.cs
+++ Project Euler/Exercise 1/Program.cs
@@ -10,31 +10,33 @@ namespace Exercise_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Exercise 1.\nGet multiples of 3 & 5 below what number?");
+            Console.WriteLine("Exercise 2.\nGet even fibonacci numbers.");
+            Console.WriteLine("Max fibonacci number?");
             string input = Console.ReadLine();
             int maxNumber;
             Int32.TryParse(input, out maxNumber);
 
-            List<int> intList = new List<int>();
-
-            for (int i = 0; i < maxNumber; i++)
-            {
-                if (i % 3 == 0 || i % 5 == 0)
-                {
-                    intList.Add(i);
-                }
-            }
-
+            List<int> intList = new List<int>() { 0, 1 };
             int total = 0;
 
-            foreach (var number in intList)
+            for (int i = 0; intList[i] < maxNumber; i++)
             {
-                total += number;
+
+                if (i == 0)
+                    continue;
+
+                int n = intList[i] + intList[i - 1];
+
+                intList.Add(n);
+
+                if (n % 2 == 0)
+                    total += n;
             }
 
             Console.WriteLine(total);
             Console.ReadLine();
 
         }
+
     }
 }
